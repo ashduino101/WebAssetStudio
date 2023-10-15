7a167be697f3e1f1930855c9227523cb73f230d8
ashduino101 <ashduino101@gmail.com>
2023-10-14 18:57:04 -0700

    fix png direction and add a preview for SkinnedMeshRenderer

--- encoders/src/pngenc.rs
+++ encoders/src/pngenc.rs
@@ -3,19 +3,36 @@ use std::panic;
 use png::Decoder;
 use wasm_bindgen::prelude::*;
 
+fn flip_v(width: usize, height: usize, data: &[u8]) -> Box<[u8]> {
+    let mut out = Vec::new();
+    for y in 0..height {
+        for x in 0..width {
+            let ry = height - y - 1;
+            out.write_all(&[
+                data[(ry * width + x) * 4],
+                data[(ry * width + x) * 4 + 1],
+                data[(ry * width + x) * 4 + 2],
+                data[(ry * width + x) * 4 + 3]
+            ]).expect("flip_v");
+        }
+    }
+    out.into()
+}
+
 #[wasm_bindgen]
 pub fn encode_png(width: u32, height: u32, data: &[u8]) -> Box<[u8]> {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
     if width == 0 || height == 0 {
         return [].into();
     }
+    let d = flip_v(width as usize, height as usize, &data);
     let mut w = Vec::new();
     let mut encoder  = png::Encoder::new(&mut w, width, height);
     encoder.set_color(png::ColorType::Rgba);
     encoder.set_depth(png::BitDepth::Eight);
     encoder.set_compression(png::Compression::Fast);
     let mut writer = encoder.write_header().unwrap();
-    let mut datavec = data[0..(width * height * 4) as usize].to_vec();
+    let mut datavec = d[0..(width * height * 4) as usize].to_vec();
     datavec.extend(std::iter::repeat(0).take(((width * height * 4) - datavec.len() as u32) as usize));
     writer.write_image_data(&datavec).unwrap();
     writer.finish().unwrap();
--- js/unityfs/classes/skinnedMeshRenderer.js
+++ js/unityfs/classes/skinnedMeshRenderer.js
@@ -1,5 +1,18 @@
 import {Renderer} from "./renderer";
 import {PPtr} from "./pptr";
+import * as THREE from "three";
+import $ from "jquery";
+import {OrbitControls} from "three/addons/controls/OrbitControls";
+import {
+  Box3,
+  Color, DirectionalLight,
+  DirectionalLightHelper,
+  Mesh,
+  MeshPhongMaterial, MeshPhysicalMaterial, MeshStandardMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer
+} from "three";
 
 export class SkinnedMeshRenderer extends Renderer {
   static exposedAttributes = [
@@ -49,5 +62,121 @@ export class SkinnedMeshRenderer extends Renderer {
     if (reader.versionGTE(4, 3)) {
       this.blendShapeWeights = reader.readArrayT(() => reader.readFloat32(), reader.readUInt32());
     }
+  }
+
+  async createPreview() {
+    this.mesh.resolve();
+    if (this.mesh.object) {
+      this.materials[0].resolve();
+      if (this.materials[0].object) {
+        const tex = this.materials[0].object.getTex('_MainTex');
+        const emission = this.materials[0].object.getTex('_EmissionMap');
+        const bump = this.materials[0].object.getTex('_BumpMap');
+        const occlusion = this.materials[0].object.getTex('_OcclusionMap');
+        const metallic = this.materials[0].object.getTex('_MetallicGlossMap');
+        if (!tex) return document.createElement('div');
+
+        const scene = new Scene();
+        scene.background = new Color(0x606060);
+
+        const loader = new THREE.TextureLoader();
+
+        const matOptions = {
+          flatShading: true,
+          map: loader.load(await tex.createDataUrl(0)),
+        };
+
+        if (emission) {
+          matOptions.emissiveIntensity = 1;
+          matOptions.emissiveMap = loader.load(await emission.createDataUrl(0));
+        }
+        if (bump) {
+          matOptions.bumpMap = loader.load(await bump.createDataUrl(0));
+          matOptions.bumpScale = 1;
+        }
+        if (occlusion) {
+          matOptions.aoMap = loader.load(await occlusion.createDataUrl(0));
+        }
+        if (metallic) {
+          matOptions.metalnessMap = loader.load(await metallic.createDataUrl(0));
+        }
+
+        const material = new MeshPhysicalMaterial(matOptions);
+
+        let mesh = new Mesh(this.mesh.object.toGeometry(), material);
+        let max = new Box3().setFromObject(mesh).max;
+        let scale = 10 / max.z;
+        mesh.rotation.set(-1.61443, 0, 0);
+        mesh.position.set(0, -4, 0);
+        mesh.scale.set(scale, scale, scale);
+
+        scene.add(mesh);
+
+        const camera = new PerspectiveCamera(70, 1, 0.01, 1000);
+
+        const renderer = new WebGLRenderer({antialias: true});
+        const prev = $('#preview');
+        renderer.setSize(prev.width(), prev.height());
+
+        const intensity = 3;
+
+        const dirLight1 = new DirectionalLight(0xffffff, intensity);
+        dirLight1.position.set(50, 50, 75);
+
+        scene.add(dirLight1);
+
+        const dirLight2 = new DirectionalLight(0xffffff, intensity);
+        dirLight2.position.set(-50, 50, 75);
+
+        scene.add(dirLight2);
+
+        const dirLight3 = new DirectionalLight(0xffffff, intensity);
+        dirLight3.position.set(0, 50, -75);
+
+        scene.add(dirLight3);
+
+        const dirLight4 = new DirectionalLight(0xffffff, intensity);
+        dirLight4.position.set(0, -75, 0);
+
+        scene.add(dirLight4);
+
+        scene.add(
+          new DirectionalLightHelper(dirLight1),
+          new DirectionalLightHelper(dirLight2),
+          new DirectionalLightHelper(dirLight3),
+          new DirectionalLightHelper(dirLight4)
+        );
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.target.set(0, 0, 0);
+        controls.update();
+
+        camera.position.set(0, 0, 30);
+
+        let lastID = 0;
+        const animate = () => {
+          renderer.render(scene, camera);
+          lastID = requestAnimationFrame(animate);
+        }
+
+        function onDestroy() {
+          document.body.removeEventListener('destroy-preview', onDestroy);
+          cancelAnimationFrame(lastID);
+        }
+        document.body.addEventListener('destroy-preview', onDestroy);
+
+        window.addEventListener('resize', function() {
+          camera.aspect = prev.width() / prev.height();
+          camera.updateProjectionMatrix();
+
+          renderer.setSize(prev.width(), prev.height());
+        }, false);
+
+        animate();
+
+        return renderer.domElement;
+      }
+    }
+    return document.createElement('div');
   }
 }
