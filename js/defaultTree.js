3f8f69065b5c0fa6128f51dc75f909d30232258d
ashduino101 <ashduino101@gmail.com>
2023-11-05 00:32:32 -0700

    fix default tree

--- js/defaultTree.js
+++ js/defaultTree.js
@@ -3,6 +3,6 @@ import {AssetTree} from "./treeview";
 export default class DefaultTree extends AssetTree {
   async loadFile() {
     // can't do much here
-    this.tree[0].children[0].innerText = 'No tree available';
+    this.tree[0].innerHTML = '<h2 class="no-preview" id="no-tree">No tree available</h2>';
   }
 }
