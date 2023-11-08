ffc74fc6ba6a1dde16ec3e8a8f7f0cbd93dccc11
ashduino101 <ashduino101@gmail.com>
2023-11-07 22:16:27 -0800

    better file input

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 import 'jstree';
 import '../css/vendor/jstree/style.min.css';
 import FileHandler from "./fileHandler";
-import {parse_xnb} from "./assetparser";
+
+async function onFile(f) {
+  const h = new FileHandler('#tree');
+  await h.loadFile(f);
+  const tree = await h.getTree();
+  document.getElementById('export-zip').onclick = () => tree.exportZip();
+}
 
 // MAIN
 function main() {
   const input = document.getElementById('file-input');
-  input.addEventListener('change', async e => {
-    let f = e.target.files[0];
-    const h = new FileHandler('#tree');
-    await h.loadFile(f);
-    const tree = await h.getTree();
-    document.getElementById('export-zip').onclick = () => tree.exportZip();
-  });
+  input.addEventListener('change', async e => onFile(e.target.files[0]));
+  const fileOverlay = document.getElementById('file-overlay');
+  document.body.addEventListener('dragover', () => fileOverlay.style.display = 'block');
+  document.body.addEventListener('dragleave', () => fileOverlay.style.display = 'none');
+  document.body.addEventListener('drop', e => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    if (files.length > 0) {
+      onFile(files[0]);
+    }
+    fileOverlay.style.display = 'none'
+  }, true);
 }
 
 // function test() {
