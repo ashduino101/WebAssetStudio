ad028eaf00e86f661e9e37c272f2586c8b9f5849
ashduino101 <ashduino101@gmail.com>
2023-11-17 20:27:07 -0800

    idk

---
+++ js/unityfs/extensionManager.js
@@ -0,0 +1,21 @@
+import {AudioClipExtension} from "./classExtensions/audioClip";
+import {FontExtension} from "./classExtensions/font";
+import {MaterialExtension} from "./classExtensions/material";
+import {MonoScriptExtension} from "./classExtensions/monoScript";
+import {TextAssetExtension} from "./classExtensions/textAsset";
+import {TransformExtension} from "./classExtensions/transform";
+import {VideoClipExtension} from "./classExtensions/videoClip";
+
+const EXTENSIONS = {
+  83: AudioClipExtension,
+  128: FontExtension,
+  21: MaterialExtension,
+  115: MonoScriptExtension,
+  49: TextAssetExtension,
+  4: TransformExtension,
+  329: VideoClipExtension
+}
+
+export class ExtensionManager {
+
+}
