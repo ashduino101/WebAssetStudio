b14e724fd3d3040130e113c747806fd0c09019c7
ashduino101 <ashduino101@gmail.com>
2025-07-01 20:44:50 -0700

    make c# decompiler smaller

--- dotnet-utils/main.js
+++ dotnet-utils/main.js
@@ -1,14 +1,35 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+console.log('Loading decompiler, please wait...');
+let bundleData = new xzwasm.XzReadableStream((await fetch("./decompiler.bin.xz")).body);
+let rdr = bundleData.getReader();
+let buf = new Uint8Array();
+while (true) {
+  let { value, done } = await rdr.read();
+  if (done) break;
+  let newBuf = new Uint8Array(buf.length + value.length);
+  newBuf.set(buf, 0);
+  newBuf.set(value, buf.length);
+  buf = newBuf;
+}
+bundleData = buf;
+console.log('Starting up...');
+const { config, dotnet } = await (
+    new (async function () {}.constructor)(
+        'data',
+        new TextDecoder('utf-8')
+        .decode(bundleData.slice(
+            new DataView(bundleData.buffer)
+                .getUint32(0, true),  // init script offset
+            bundleData.length
+        ))
+    )(bundleData)
+);
 
-import { dotnet } from './dotnet.js'
+const { getAssemblyExports, runMain } = await dotnet
+.withDiagnosticTracing(false)
+.withConfig(config)
+.withApplicationArgumentsFromQuery()
+.create();
 
-const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
-    .withDiagnosticTracing(false)
-    .withApplicationArgumentsFromQuery()
-    .create();
-
-const config = getConfig();
 const exports = window.exports = await getAssemblyExports(config.mainAssemblyName);
 
 const input = document.getElementById('file-input');
@@ -23,9 +44,18 @@ input.addEventListener('change', e => {
     // let types = JSON.parse(exports.Decompiler.GetTopLevelTypes(decompiler, modules[0].assemblyName));
     // console.log(types);
     console.log('done');
+
+    let types = new Set();
+    for (const module of modules) {
+      for (const type of module.types) {
+        if (types.has(type)) continue;
+        console.log(exports.Decompiler.DecompileTypeAsString(decompiler, type));
+        types.add(type);
+      }
+    }
     // document.getElementById('out').textContent = text;
   }
   reader.readAsArrayBuffer(f);
 });
 
-await dotnet.run();
+await runMain();
